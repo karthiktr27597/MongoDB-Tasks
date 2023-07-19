462f3dfe8f486fd2560b91a1da4f5946cf13ef0b
karthiktr27597 <karthiktr27597@gmail.com>
2023-07-20 02:12:56 +0530

    task2 updated

---
+++ Tasks/MongoDB-Task2.js
@@ -0,0 +1,524 @@
+// Design database for Zen class programme
+// users
+
+users = [
+    {
+        "id": "100",
+        "name": "Anbu",
+        "batch": "b40WD"
+    },
+    {
+        "id": "101",
+        "name": "Aravind",
+        "batch": "b41WD"
+    },
+    {
+        "id": "102",
+        "name": "Bhuvana",
+        "batch": "b42WD"
+    },
+    {
+        "id": "103",
+        "name": "Bharath",
+        "batch": "b43WD"
+    },
+    {
+        "id": "104",
+        "name": "Chandru",
+        "batch": "b44WD"
+    },
+    {
+        "id": "105",
+        "name": "Chitharanjan",
+        "batch": "b40WD"
+    },
+    {
+        "id": "106",
+        "name": "Charles",
+        "batch": "b41WD"
+    },
+    {
+        "id": "107",
+        "name": "Ajith K",
+        "batch": "b42WD"
+    },
+    {
+        "id": "108",
+        "name": "AjithKumar P S",
+        "batch": "b43WD"
+    },
+    {
+        "id": "109",
+        "name": "Dinesh",
+        "batch": "b44WD"
+    },
+    {
+        "id": "110",
+        "name": "Elango",
+        "batch": "b40WD"
+    },
+    {
+        "id": "111",
+        "name": "Ganesh",
+        "batch": "b41WD"
+    },
+    {
+        "id": "112",
+        "name": "Gunasekar",
+        "batch": "b42WD"
+    },
+    {
+        "id": "113",
+        "name": "Harish",
+        "batch": "b43WD"
+    },
+    {
+        "id": "114",
+        "name": "Hari Krishnan",
+        "batch": "b44WD"
+    },
+    {
+        "id": "115",
+        "name": "ilakiya",
+        "batch": "b40WD"
+    },
+    {
+        "id": "116",
+        "name": "Jeyanthi",
+        "batch": "b41WD"
+    },
+    {
+        "id": "117",
+        "name": "Jeyam",
+        "batch": "b42WD"
+    },
+    {
+        "id": "118",
+        "name": "Jeyam Ravi",
+        "batch": "b43WD"
+    },
+    {
+        "id": "119",
+        "name": "Krish",
+        "batch": "b44WD"
+    },
+    {
+        "id": "120",
+        "name": "Karthik",
+        "batch": "b40WD"
+    },
+    {
+        "id": "121",
+        "name": "Kumar",
+        "batch": "b41WD"
+    },
+    {
+        "id": "122",
+        "name": "Mani",
+        "batch": "b42WD"
+    },
+    {
+        "id": "123",
+        "name": "Nagaraj",
+        "batch": "b43WD"
+    },
+    {
+        "id": "124",
+        "name": "Priya",
+        "batch": "b44WD"
+    },
+]
+
+codekata =
+    [
+        {
+            "id": "100",
+            "codekata": 90
+        },
+        {
+            "id": "101",
+            "codekata": 150
+        },
+        {
+            "id": "102",
+            "codekata": 200
+        },
+        {
+            "id": "103",
+            "codekata": 220
+        },
+        {
+            "id": "104",
+            "codekata": 95
+        },
+        {
+            "id": "105",
+            "codekata": 150
+        },
+        {
+            "id": "106",
+            "codekata": 180
+        },
+        {
+            "id": "107",
+            "codekata": 200
+        },
+        {
+            "id": "108",
+            "codekata": 203
+        },
+        {
+            "id": "109",
+            "codekata": 177
+        },
+        {
+            "id": "110",
+            "codekata": 191
+        },
+        {
+            "id": "111",
+            "codekata": 133
+        },
+        {
+            "id": "112",
+            "codekata": 173
+        },
+        {
+            "id": "113",
+            "codekata": 133
+        },
+        {
+            "id": "114",
+            "codekata": 181
+        },
+        {
+            "id": "115",
+            "codekata": 110
+        },
+        {
+            "id": "116",
+            "codekata": 240
+        },
+        {
+            "id": "117",
+            "codekata": 201
+        },
+        {
+            "id": "118",
+            "codekata": 190
+        },
+        {
+            "id": "119",
+            "codekata": 207
+        },
+        {
+            "id": "120",
+            "codekata": 192
+        },
+        {
+            "id": "121",
+            "codekata": 144
+        },
+        {
+            "id": "122",
+            "codekata": 149
+        },
+        {
+            "id": "123",
+            "codekata": 187
+        },
+        {
+            "id": "124",
+            "codekata": 192
+        },
+    ]
+attendance =
+    [
+        {
+            "id": "100",
+            "name": "Anbu",
+            "date": '16-oct-2020',
+            "attendance_status": "absent"
+        },
+        {
+            "id": "100",
+            "name": "Anbu",
+            "date": '18-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "100",
+            "name": "Anbu",
+            "date": '20-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "100",
+            "name": "Anbu",
+            "date": '25-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "100",
+            "name": "Anbu",
+            "date": '30-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "101",
+            "name": "Aravind",
+            "date": '16-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "101",
+            "name": "Aravind",
+            "date": '18-oct-2020',
+            "attendance_status": "absent"
+        },
+        {
+            "id": "101",
+            "name": "Aravind",
+            "date": '20-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "101",
+            "name": "Aravind",
+            "date": '25-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "101",
+            "name": "Aravind",
+            "date": '30-oct-2020',
+            "attendance_status": "absent"
+        },
+        {
+            "id": "103",
+            "name": "Bharath",
+            "date": '14-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "103",
+            "name": "Bharath",
+            "date": '18-oct-2020',
+            "attendance_status": "absent"
+        },
+        {
+            "id": "103",
+            "name": "Bharath",
+            "date": '20-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "103",
+            "name": "Bharath",
+            "date": '25-oct-2020',
+            "attendance_status": "present"
+        },
+        {
+            "id": "103",
+            "name": "Bharath",
+            "date": '30-oct-2020',
+            "attendance_status": "absent"
+        }
+    ]
+
+topics = [
+    {
+        "topic": ["Javascript", "DS&Alog"],
+        "month": "September"
+    },
+    {
+        "topic": ["HTML", "CSS"],
+        "month": "October"
+    },
+    {
+        "topic": ["Reactjs", "MySQL"],
+        "month": "November"
+    },
+    {
+        "topic": ["MongoDB", "Expressjs"],
+        "month": "December"
+    }
+]
+
+tasks = [
+    {
+        "Task": ["Palindrome", "print odd number", "Any one sum in IIFE function", "Sum of all numbers", "Return median of two sorted arrays"],
+        "month": "September"
+    },
+    {
+        "Task": ["Basic HTML page creaton", "CSS Task of a webpage", "Create a webpage use only HTML&CSS"],
+        "month": "October",
+        "task_submitted": [100, 102],
+        "task_pending": [101, 103]
+    },
+    {
+        "Task": ["React AddtoCard", "CRUD Operation in React", "MySQL Bold Queries completion"],
+        "month": "November"
+    },
+    {
+        "Task": ["MongoDB new Database Creation", "Fetch Data in MongoDB", "Query Handling"],
+        "month": "December"
+    }
+]
+company_drives = [
+    {
+        "company": "TCS",
+        "date": "16-oct-2020",
+        "attendance_status": "present"
+    },
+    {
+        "company": "HCL",
+        "date": "18-oct-2020",
+        "attendance_status": "present"
+    },
+    {
+        "company": "Accenture",
+        "date": "14-oct-2020",
+        "attendance_status": "present"
+    },
+    {
+        "company": "Startup Company",
+        "date": "16-oct-2020",
+        "attendance_status": "absent"
+    },
+    {
+        "company": "Wipro",
+        "date": "25-oct-2020",
+        "attendance_status": "present"
+    }
+]
+mentors = [
+    {
+        "name": "Sanjay",
+        "batch": "B40WE",
+        "mentee": 25
+
+    },
+    {
+        "name": "Ragav",
+        "batch": "B41WE",
+        "mentee": 15
+
+    },
+    {
+        "name": "Sangeetha",
+        "batch": "B42WE",
+        "mentee": 14
+
+    },
+    {
+        "name": "Farook",
+        "batch": "B43WE",
+        "mentee": 18
+
+    },
+    {
+        "name": "Magesh",
+        "batch": "B44WE",
+        "mentee": 21
+
+    }
+]
+
+
+// 1. Find all the topics and tasks which are thought in the month of October
+
+Ans: db.topics.aggregate([{ $lookup: { from: "tasks", localField: "month", foreignField: "month", as: "October_tasks" } }, { $match: { month: "October" } }]);
+
+[
+    {
+        _id: ObjectId("64914bcda98d7081b34b5edb"),
+        topic: ['HTML', 'CSS'],
+        month: 'October',
+        October_tasks: [
+            {
+                _id: ObjectId("64886f07cb017f76a537d536"),
+                Task: [
+                    'API Task',
+                    'Async and Await Task',
+                    'Basic HTML page creaton',
+                    'CSS Task of a webpage'
+                ],
+                month: 'October'
+            }
+        ]
+    }
+]
+
+// 2. Find all the company drives which appeared between 15 oct - 2020 and 31 - oct - 2020
+
+Ans: db.company_drives.aggregate({ $match: { "date": { $gt: "15-oct-2020", $lt: "30-oct-2020" } } }, { $match: { "attendance_status": { $eq: "present" } } }).toArray();
+
+// 3. Find all the company drives and students who are appeared for the placement.
+
+Ans: db.company_drives.aggregate([{ $lookup: { from: "attendance", localField: "date", foreignField: "date", as: "company_drives" } },
+{ $match: { attendance_status: "present" } }]).forEach((doc) => {
+    print("\ncompany:", doc.company, "\ndate:", doc.date, "\nstudent_status:"),
+        doc.company_drives.forEach((doc) => { print(doc) })
+}).toArray();
+
+// 4. Find the number of problems solved by the user in codekata
+
+Ans: db.codekata.aggregate(
+    [{
+        $lookup: {
+            from: "users",
+            localField: "id",
+            foreignField: "id",
+            as: "Solved_ByUser",
+        }
+    }
+    ]).toArray();
+
+// 5. Find all the mentors with who has the mentee's count more than 15
+
+Ans: db.mentors.find({ mentee: { $gt: 15 } });
+
+// 6. Find the number of users who are absent and task is not submitted  between 15 oct - 2020 and 31 - oct - 2020
+
+Answer:
+
+// finding pending task users
+
+db.tasks.find({ month: "October" }).forEach((val) => { val.task_pending.forEach((id) => { print({ "id": id }) }) }); // Ans:  { id: 101 } { id: 103 }
+
+// insert to new collection for aggregate with attendance
+
+db.taskpending.insertMany([{ id: '101' }, { id: '103' }]);   // Ans: [{ _id: ObjectId("64b64ef6d260185ff83b4588"), id: 101 },{ _id: ObjectId("64b64ef6d260185ff83b4589"), id: 103 }]
+
+
+// final answer 
+
+db.attendance.aggregate([
+    {
+        $match: {
+            date: {
+                $gte: "15-oct-2020", $lte: "31-oct-2020"
+            }
+        }
+    },
+    { $match: { attendance_status: "absent" } },
+    {
+        $lookup: {
+            from: "taskpending",
+            localField: "id",
+            foreignField: "id",
+            as: "taskpending_users"
+        }
+    },
+    {
+        $group: {
+            _id: "$taskpending_users.id",
+        }
+
+    }, {
+        $match: {
+            _id: { $gt: [] }
+        }
+    }
+]).toArray();
